8cd874ae09989b55fe43da5e7a1b7420fcdaf65f
JoaquinCorcheroFMP <jcorchero@findmypast.com>
2017-01-23 08:59:36 +0000

    (p21) added props and passed values to child component

--- voting_app/app.js
+++ voting_app/app.js
@@ -1,33 +1,52 @@
 const ProductList = React.createClass({
     render: () => {
+        const product = Data[0];
         return(
             <div className='ui items'>
-                <Product />
+                <Product
+                    id={product.id}
+                    title={product.title}
+                    description={product.description}
+                    url={product.url}
+                    votes={product.votes}
+                    submitter_avatar_url={product.submitter_avatar_url}
+                    product_image_url={product.product_image_url}
+                />
             </div>
         );
     }
 });
 
 const Product = React.createClass({
-    render: () => {
-        return(
+    render: function () {
+        return (
             <div className='item'>
                 <div className='image'>
-                    <img src='images/products/image-aqua.png' />
+                 <img src={this.props.product_image_url} />
                 </div>
                 <div className='middle aligned content'>
+                    <div className='header'>
+                        <a>
+                            <i className='large caret up icon'></i>
+                        </a>
+                        {this.props.votes}
+                    </div>
                     <div className='description'>
-                        <a>Fort Knight</a>
-                        <p>Authentic renaissance actors, delivered in just two weeks.</p>
+                        <a href={this.props.url}>
+                            {this.props.title}
+                        </a>
                     </div>
                     <div className='extra'>
                         <span>Submitted by:</span>
-                        <img className='ui avatar image' src='images/avatars/daniel.jpg' />
+                        <img
+                            className='ui avatar image'
+                            src={this.props.submitter_avatar_url}
+                        />
                     </div>
                 </div>
             </div>
         );
-    }
+    },
 });
 
 ReactDOM.render(
