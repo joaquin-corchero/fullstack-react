9bbd7c2977cbcdc51b350539d66b12f39c172ea8
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-01-26 11:36:47 +0000

    (p80) added deletion

--- time_tracking_app/public/app.js
+++ time_tracking_app/public/app.js
@@ -45,6 +45,14 @@ const TimerDashboard = React.createClass({
             })
         });
     },
+    handleTrashClick: function(timerId){
+        this.deleteTimer(timerId)
+    },
+    deleteTimer: function(timerId){
+        this.setState({
+            timers: this.state.timers.filter(t => t.id !== timerId),
+        });
+    },
     render: function() {
         return (
             <div className='ui three column centered grid'>
@@ -52,6 +60,7 @@ const TimerDashboard = React.createClass({
                     <EditableTimerList
                         timers={this.state.timers}
                         onFormSubmit={this.handleEditFormSubmit}
+                        onTrashClick={this.handleTrashClick}
                     />
                     <ToggleableTimerForm
                         onFormSubmit={this.handleCreateFormSubmit}
@@ -74,6 +83,7 @@ const EditableTimerList = React.createClass({
                 elapsed={timer.elapsed}
                 runningSince={timer.runningSince}
                 onFormSubmit={this.props.onFormSubmit}
+                onTrashClick={this.props.onTrashClick}
             />
         ));
         return (
@@ -126,6 +136,7 @@ const EditableTimer = React.createClass({
                     elapsed={this.props.elapsed}
                     runningSince={this.props.runningSince}
                     onEditClick={this.handleEditClick}
+                    onTrashClick={this.props.onTrashClick}
                 />
             );
         }
@@ -221,6 +232,9 @@ const ToggleableTimerForm = React.createClass({
 });
 
 const Timer = React.createClass({
+    handleTrashClick: function(){
+        this.props.onTrashClick(this.props.id);
+    },
     render: function(){
         const elapsedString = helpers.renderElapsedString(this.props.elapsed);
         return (
@@ -242,7 +256,10 @@ const Timer = React.createClass({
                         >
                             <i className='edit icon'></i>
                         </span>
-                        <span className='right floated trash icon'>
+                        <span 
+                            className='right floated trash icon'
+                            onClick={this.handleTrashClick}
+                        >
                             <i className='trash icon'></i>
                         </span>
                     </div>
