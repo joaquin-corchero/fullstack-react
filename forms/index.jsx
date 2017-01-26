eab6028d97ea5c0d588993687ef65c40c31e7133
Joaquin Corchero <joaquin.corchero@gmail.com>
2017-01-26 16:23:45 +0000

    (p165) Added the basic-input and basic-button

---
+++ forms/index.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import One from './basic-button.jsx';
+import Two from './basic-input.jsx';
+const routes = [
+  One, Two
+];
+
+const location = window.location;
+window.addEventListener('hashchange', location.reload.bind(location));
+const loc = location.hash.replace('#/', '');
+const element = loc ? createRoute(loc) : createTOC();
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+
+render(element, container);
+
+function createTOC() {
+  return (
+    <div>
+      {routes.map((route, i) =>
+        <li key={i}><a href={`#/${(i + 1)}`}>{route.displayName}</a></li>
+      )}
+    </div>
+  );
+}
+
+function createRoute(nStr) {
+  let i = nStr - 1;
+  if (i < 0) i = 0;
+  if (i > routes.length - 1) i = routes.length - 1;
+
+  const navStyle = {
+    position: 'fixed', bottom: 50, width: '100%', textAlign: 'center',
+  };
+  const isFirst = i <= 0;
+  const isLast = i >= routes.length - 1;
+
+  return (
+    <div>
+      {React.createElement(routes[i])}
+      <div style={navStyle}>
+        { isFirst ? '' : <a href={`#/${i}`}>{'<'}</a> }
+        <a href={location.href.replace(location.hash, '')}>TOC</a>
+        { isLast ? '' : <a href={`#/${(i + 2)}`}>{' > '}</a> }
+      </div>
+    </div>
+  );
+}
