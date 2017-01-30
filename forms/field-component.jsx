fe454cc782f185f56550d4ce02ee8bfe42a2b1b9
JoaquinCorcheroFMP <jcorchero@findmypast.com>
2017-01-30 08:25:06 +0000

    (p180) can get the sate update from parent, can validate field

--- forms/field-component.jsx
+++ forms/field-component.jsx
@@ -14,4 +14,29 @@ module.exports = React.createClass({
       error: false
     }
   },
+  componentWillReceiveProps(update){
+    this.setState({value: update.value});
+  },
+  render(){
+    return(
+      <div>
+        <input
+          placeholder={this.props.placeHolder}
+          value={this.props.value}
+          onChange={this.props.onChange}
+        />
+        <span style={{color:'red'}}>{this.state.error}</span>
+      </div>
+    );
+  },
+  onChange(evt){
+    const name = this.props.name;
+    const value = this.props.value;
+    //If there was a validation method passed in it gets validated, otherwise no error set
+    const error = this.props.validate ? this.props.validate(value) : false;
+
+    this.setState({value, error});
+
+    this.props.onChange({name, value, error});
+  }
 });
